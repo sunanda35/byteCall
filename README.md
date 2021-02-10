c5ac6b7faa7b84f156491c4e829c2bba211035ef
sunanda <sunandasamanta38@gmail.com>
2021-02-11 03:51:05 +0530

    create a simple api

---
+++ README.md
@@ -0,0 +1,2 @@
+# It's a socket using project.
+## make video calling app using my api, its happen using socket.io
--- index.js
+++ index.js
@@ -1,11 +1,15 @@
-var express = require('express');
+var express = require('express');     //import express
+require('dotenv').config()          //using .env file
 var app = express();
 
-const PORT = process.env.PORT || 3000;;
+const PORT = process.env.PORT || 3000;          //configure port like (localhost:3000)
 
 app.get('/', (req, res)=>{
     res.send('hello world bitch');
 })
 
 
-app.listen(PORT, ()=>console.log(`Server started at: http://127.0.0.1:${PORT}`))
+app.listen(PORT, 
+    ()=>{
+        console.log(`Server started at: http://127.0.0.1:${PORT}`);
+    })    // it the main connection with server
